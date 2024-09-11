fc56dc5e7bc4a361124db81d56472cbc65dbe5a7
James Arn <132308884+arnsocks@users.noreply.github.com>
2024-09-10 20:47:48 -0400

    Create the primary operation functions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,35 @@
-body = document.querySelector("body");
+const operate = function(a, operation, b) {
+  switch(true) {
+    case (operation === 'add'):
+      console.log(`We are adding.`);
+      return add(a, b);
+    case (operation === 'subtract'):
+      console.log('We are subtracting.');
+      return subtract(a,b);
+    case (operation === 'multiply'):
+      console.log('We are multiplying');
+      return multiply(a,b);
+    case (operation === 'divide'):
+      console.log("We are dividing");
+      return divide(a,b);
+    default:
+      console.log("I do not recognize that operation");
+      return "Error: operation not recognized";
+  }
+}
 
-body.style.color = "red";
+const add = function(a,b) {
+  return (a + b);
+}
+
+const subtract = function(a,b) {
+  return (a - b);
+}
+
+const multiply = function(a,b) {
+  return (a * b);
+}
+
+const divide = function(a,b) {
+  return b == 0 ? `I can't do that, Dave` : (a / b);
+}
