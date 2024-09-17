544dc5d74a5e4557cd8d46842ee9e5ed380de2aa
James Arn <132308884+arnsocks@users.noreply.github.com>
2024-09-16 21:15:00 -0400

    Fix bugs related to pressing the equals key
    - Pressing a number after equals now correctly
      resets everything but the display value.
    - Pressing equals 'early' no longer brakes it

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ let firstOperand = null;
 let firstOperation = null;
 let secondOperand = null;
 let result = null;
+let hitEquals = false;
 
 const display = document.querySelector("#output");
 
@@ -14,15 +15,24 @@ const equalsButton = document.querySelector('#equals');
 for (button of operandButtons) {
   button.addEventListener("click", (e) => {
     console.log(`You clicked the ${e.target.textContent} button`);
-    displayValue = displayValue * 10 + parseInt(e.target.textContent);
+
+    let keyValue = parseInt(e.target.textContent);
+
+    if (hitEquals) {
+      firstOperand = null;
+      hitEquals = false;
+    }
+    
+    displayValue = displayValue * 10 + keyValue;
     display.textContent = displayValue;
+    
   })
 }
 
 for (button of operationButtons) {
   button.addEventListener("click", (e) => {
     console.log(`You clicked the ${e.target.textContent} button`);
-
+    hitEquals = false;
     if (!firstOperand) {
       firstOperand = displayValue;
       firstOperation = e.target.id;
@@ -33,16 +43,26 @@ for (button of operationButtons) {
       firstOperand = result;
       display.textContent = result;
       displayValue = 0;
-      firstOperation = null;
       secondOperand = null;
-
-    }
-    if (e.target.id != 'equals') {
       firstOperation = e.target.id;
-      console.log("You didn't click equals");
     }
   })
 }
+
+equalsButton.addEventListener('click', () => {
+  if (firstOperation) {
+    secondOperand = displayValue;
+    result = operate(firstOperation, firstOperand, secondOperand);
+    firstOperand = result;
+    display.textContent = result;
+    displayValue = 0;
+    firstOperation = null;
+    secondOperand = null;
+    hitEquals = true;
+  } else {
+    //displayValue = 0;
+  }
+})
 
 clearButton.addEventListener('click', () => {
   displayValue = 0;
